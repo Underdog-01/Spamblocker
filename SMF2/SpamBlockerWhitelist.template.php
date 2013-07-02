3678b91a38ac9c7f97b34b32ce1845c1a7b4890f
Underdog <github.underdog@gmail.com>
2013-07-02 15:12:27 -0400

    Update SpamBlockerWhitelist.template.php

--- SMF2/SpamBlockerWhitelist.template.php
+++ SMF2/SpamBlockerWhitelist.template.php
@@ -3,14 +3,14 @@
 
 /*
  *	Main admin whitelist template file for the Spam Blocker Mod	
- *	c/o Underdog @ http://askusaquestion.net			
+ *	c/o Underdog @ http://webdevelop.comli.com			
 */
 
 /*
- * Spam Blocker was developed for SMF forums c/o Underdog @ http://askusaquestion.net	
- * Copyright 2013 Underdog@askusaquestion.net
+ * Spam Blocker was developed for SMF forums c/o Underdog @ http://webdevelop.comli.com	
+ * Copyright 2013 underdog@webdevelop.comli.com
  * This software package is distributed under the terms of its Freeware License
- * http://askusaquestion.net/index.php/page=spamblocker_license
+ * http://webdevelop.comli.com/index.php/page=spamblocker_license
 */
 
 /* Spam Blocker whitelist display  */
